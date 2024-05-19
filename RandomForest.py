6b9c96fa116ff0dd9c29d7a61ca6f48c298515dd
Myriuss <marielelyamani@gmail.com>
2024-05-19 11:25:29 +0200

    :white_check_mark: Prediction sur 3ans

--- RandomForest.py
+++ RandomForest.py
@@ -8,19 +8,22 @@ df = pd.read_csv("resultat_jointure.csv")
 # 2. Sélectionner les fonctionnalités pertinentes
 features = df[['2017', '2018', '2019', '2020', '2021', '2022', 'Hommes_0-19', 'Hommes_20-39', 'Hommes_40-59', 'Hommes_60-74', 'Hommes_75plus', 'Femmes_0-19', 'Femmes_20-39', 'Femmes_40-59', 'Femmes_60-74', 'Femmes_75plus']]
 
-# La cible de notre modèle sera le parti politique vainqueur en 2022
-target = df['parti_politique_vainq2022']
-
-# 3. Diviser les données en ensembles de formation et de test
-X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2, random_state=42)
+# La cible de notre modèle sera le parti politique vainqueur en 2017
+target = df['parti_politique_vainq2017']
 
 # 4. Entraîner un modèle de Random Forest
 rf_model = RandomForestClassifier(n_estimators=100, random_state=42)
-rf_model.fit(X_train, y_train)
+rf_model.fit(features, target)
 
-# 5. Effectuer des prédictions sur toutes les données
-all_predictions = rf_model.predict(features)
+# 5. Obtenir les probabilités pour chaque classe cible (parti politique) pour chaque département
+probabilities = rf_model.predict_proba(features)
 
-# 6. Stocker les prédictions dans un nouveau fichier CSV
-predictions_df = pd.DataFrame({'Libelle_departement': df['Libelle_departement'], 'Prediction': all_predictions})
-predictions_df.to_csv('prediction.csv', index=False)
+# 6. Créer un DataFrame pour stocker les probabilités
+probabilities_df = pd.DataFrame(probabilities, columns=rf_model.classes_)
+
+# Ajouter la colonne des départements au DataFrame
+probabilities_df.insert(0, 'Libelle_departement', df['Libelle_departement'])
+
+# 7. Stocker les probabilités dans un nouveau fichier CSV
+probabilities_df.to_csv('probabilities.csv', index=False)
+
